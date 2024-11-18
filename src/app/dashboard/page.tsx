2b3bc4670a06753d7cfbd3ce34cc46a57fd20fab
Dev-scott <sadoscott25@gmail.com>
2024-11-18 23:27:29 +0100

    refactor app

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { DashboardPage } from "@/components/dashboard-page"
 import { db } from "@/db"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
+import { DashboardPageContent } from "./(components)/dashboard-page-content"
 
 const Page = async () => {
     const auth = await currentUser()
