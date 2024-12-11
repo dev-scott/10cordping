354bf1ed6894699e054650aba60fa1405c08df46
Dev-scott <sadoscott25@gmail.com>
2024-12-11 11:43:45 +0100

    add configuration for github page deployement

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: '/your-repo-name',
+  images: {
+    unoptimized: true
+  }
+}
 
 export default nextConfig
